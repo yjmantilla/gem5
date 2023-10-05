88af333124ec9f74f0fb4853eb062299f4bf5349
yjmantilla <yjmantilla@gmail.com>
2023-10-05 12:48:32 -0500

    power cmds

---
+++ powerCommands.sh
@@ -0,0 +1,25 @@
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@256kB/stats.txt gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@256kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@256kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@256kB/config.xml > gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@256kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@256kB/stats.txt gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@256kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@256kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@256kB/config.xml > gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@256kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@128kB/stats.txt gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@128kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@128kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@128kB/config.xml > gem5/output/wl@jpeg2k_dec.l1i_size@64kB.l1d_size@128kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@128kB/stats.txt gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@128kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@128kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@128kB/config.xml > gem5/output/wl@h264dec.l1i_size@64kB.l1d_size@128kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@256kB/stats.txt gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@256kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@256kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@256kB/config.xml > gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@256kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@256kB/stats.txt gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@256kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@256kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@256kB/config.xml > gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@256kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@128kB/stats.txt gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@128kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@128kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@128kB/config.xml > gem5/output/wl@jpeg2k_dec.l1i_size@32kB.l1d_size@128kB/power.log ) &
+(
+python gem5/gem5toMcPAT_cortexA76.py gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@128kB/stats.txt gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@128kB/config.json gem5/ARM_A76_2.1GHz.xml gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@128kB/config.xml
+./mcpat/mcpat -infile gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@128kB/config.xml > gem5/output/wl@h264dec.l1i_size@32kB.l1d_size@128kB/power.log ) &
+pwd
